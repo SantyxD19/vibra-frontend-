8fe3b478b2bf3033fb1efb956bfa79af49b115d3
Santy Macea <santiagomacea19@gmail.com>
2026-05-02 02:02:48 -0500

    add logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
       <div className="flex justify-between items-center">
         {/* LOGO */}
         <div className="flex items-center gap-2">
-          <img src={logo} alt="Vibra" className="h-18" />
+          <img src={logo} alt="Vibra" className="h-15" />
         </div>
 
         {/* BOTÓN MOBILE */}
