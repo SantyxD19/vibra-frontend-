9de213f6d890b5fb7ba2c0236ab7c365057d7b5d
Santy Macea <santiagomacea19@gmail.com>
2026-05-04 00:24:40 -0500

    logo en login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -97,7 +97,7 @@ function Login() {
       <div className="hidden md:flex w-1/2 bg-black text-white items-center justify-center">
         <div>
           <div className="flex justify-center mb-4">
-            <img src={logo} className="h-24 border border-red-500" />
+            <img src={logo} className="h-40 md:h-52 object-contain" />
           </div>
           <p className="text-gray-400 mt-3">
             Conecta con eventos y combos en tiempo real
